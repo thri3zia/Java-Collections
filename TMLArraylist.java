d86ce847457d81447fa5e59cbf05e3a14e57766a
codewithjuspher <112837023+thri3zia@users.noreply.github.com>
2022-09-29 11:28:27 +0800

    TMLArrayList changes

--- TMLArraylist.java
+++ TMLArraylist.java
@@ -9,6 +9,7 @@ public class TMLArraylist {
         
         Scanner input = new Scanner(System.in);
         Scanner in = new Scanner(System.in);
+        int number = 0;
 
         
         ArrayList <Integer> numberArrayList = new ArrayList<>();
@@ -18,31 +19,36 @@ public class TMLArraylist {
         numberArrayList.add(2);
         numberArrayList.add(5);
         char choice;
-        
-        System.out.println("Given numbers in ArrayList implementation:" + numberArrayList);
-        System.out.println("Choose from below:");
-        System.out.print("\n1. Add");
-        System.out.print("\n2. Remove");
-        System.out.print("\n3. Display");
-        System.out.print("\n4. Exit");
-        
-	System.out.print("\nSelected option: ");
-        choice = input.next().charAt(0);
-           
-        switch (choice) {
+
+        do{
+            System.out.println("\n===================================================================");
+            System.out.println("Given numbers in ArrayList implementation:" + numberArrayList);
+            System.out.println("Choose from below:");
+            System.out.print("1. Add");
+            System.out.print("\n2. Remove");
+            System.out.print("\n3. Display");
+            System.out.print("\n4. Clear");
+            System.out.print("\n5. Exit");
+
+            System.out.print("\nSelected option: ");
+            choice = input.next().charAt(0);
+            
+           switch (choice) {
                  
                 case '1':
                     System.out.println("\n***** You are now going to ADD a NUMBER *****");
-                        int number = 0;
+                        number = 0;
                         System.out.print("\nInput a number: ");
                         number = in.nextInt();
           //---------- CONDITION ADDED ---------------
                     if (number >= 0){
                         numberArrayList.add(number);
-                        System.out.println("The added number is:" + numberArrayList.get(5));
+                        System.out.println("\nThe added number is: " + numberArrayList.get(5));
                         System.out.println(numberArrayList);
+                        System.out.print("\n********************************************************************");
                     }else{
                         System.out.print("\nPlease input a POSITIVE NUMBER\n");
+                        System.out.print("\n********************************************************************");
                     }
         //--------------------------------------------
                     break;
@@ -51,24 +57,33 @@ public class TMLArraylist {
                     System.out.print("\nInput the index you want to remove: ");
                     int number2 = in.nextInt();
                     numberArrayList.remove(number2);
-                    System.out.println("The removed index number is:" + number2);
+                    System.out.println("\nThe removed index number is: " + number2);
                     System.out.println("The latest ArrayList:" + numberArrayList);
+                    System.out.print("\n********************************************************************");
                     break;
                 case '3':
-                    System.out.print("\nInput a number: ");               
-                    number = in.nextInt();
-                    numberArrayList.add(number);
-                    System.out.println("The added number is:" + numberArrayList.get(5));
+                    System.out.println("\nThe added number earlier is: " + number);
                     System.out.println(numberArrayList);
+                    System.out.print("\n********************************************************************");
                     break;
                 case '4':
-                    System.out.println("\n******* THANK YOU FOR USING THIS SIMPLE PROGRAM. ADIOS! *******");   
+                    System.out.print("\n********************************************************************\n"); 
+                    numberArrayList.clear();  
+                    System.out.println("\n\t\tCLEARED HashSet Collection:" + numberArrayList);
+                    System.out.print("\n********************************************************************\n"); 
                     break;
+                case '5':
+                    System.out.print("\n====================================================================\n"); 
+                    System.out.println("\n******* THANK YOU FOR USING THIS SIMPLE PROGRAM. ADIOS! *******");  
+                    System.out.print("\n====================================================================\n\n"); 
+                    break;
+                
                 default:
                     System.out.println("\nJust Pick fron the options given above. Please try Again. ");
                     break;
-            }
-            
+            } 
+        }while (choice != '5');
+
     }
     
 }
